32bed668838fd9bbc086d94c359a1598b4b106ae
php-user1 <hayksg@gmail.com>
2018-10-13 19:25:01 -0400

    Post Request

--- app/Services/v1/FlightService.php
+++ app/Services/v1/FlightService.php
@@ -3,6 +3,7 @@
 namespace App\Services\v1;
 
 use App\Flight;
+use App\Airport;
 
 class FlightService
 {
@@ -89,5 +90,29 @@ class FlightService
         }
 
         return $data;
+    }
+
+    public function createFlight($req)
+    {
+        $arrivalAirport = $req->input('arrival.iataCode');
+        $departureAirport = $req->input('departure.iataCode');
+
+        $airports = Airport::whereIn('iataCode', [$arrivalAirport, $departureAirport])->get();
+        $codes = [];
+
+        foreach ($airports as $airport) {
+            $codes[$airport->iataCode] = $airport->id;
+        }
+
+        $flight = new Flight();
+        $flight->flightNumber = $req->input('flightNumber');
+        $flight->status = $req->input('status');
+        $flight->arrivalAirport_id = $codes[$arrivalAirport];
+        $flight->arrivalDateTime = $req->input('arrival.datetime');
+        $flight->departureAirport_id = $codes[$departureAirport];
+        $flight->departureDateTime = $req->input('departure.datetime');
+
+        $flight->save();
+        return $this->filterFlights([$flight]);
     }
 }
